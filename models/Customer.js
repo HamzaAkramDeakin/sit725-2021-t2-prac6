60241f72eba05cacc3ade3ab76f90fbfe9238882
Hamza Akram <hamzaakram66@gmail.com>
2021-10-09 09:52:38 +0530

    Added Customer model

---
+++ models/Customer.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.models.Service || mongoose.model("Customer", schema);
